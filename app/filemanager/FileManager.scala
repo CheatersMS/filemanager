8ea9b33179e14f3520ddcfee10e05d60b025ab99
TaeHwanLee <thlee@manaolabs.com>
2012-12-14 15:27:17 +0900

    Add: Some method to resize an image

--- app/filemanager/FileManager.scala
+++ app/filemanager/FileManager.scala
@@ -5,13 +5,55 @@ import scala.concurrent._
 import scala.concurrent.ExecutionContext.Implicits.global
 import play.api.libs.Files.TemporaryFile
 
+import java.util.UUID
+import java.awt.image.BufferedImage
+import javax.imageio.ImageIO
+import java.io._
+import org.imgscalr.Scalr
+
 object FileManager {
-  def getStorage(storage:String = "s3",bucketName:String):Storage = storage.toLowerCase match {
+  def getStorage(storage: String = "s3", bucketName: String): Storage = storage.toLowerCase match {
     case "s3" => S3Storage(bucketName)
   }
 
-  def add(filePart: FilePart[TemporaryFile], path: String = "")(implicit storage: Storage): Future[String] = {
+
+  def resizeImage(filePart: FilePart[TemporaryFile], maxium_width_and_height:Int): Future[Array[Byte]] = {
+    val file = filePart.ref.file
+    val contentType: String = filePart.contentType.getOrElse("application/octet-stream")
+
+    resizeImage(contentType, file, maxium_width_and_height)
+  }
+
+  def resizeImage(contentType: String, file: File, maxium_width_and_height:Int): Future[Array[Byte]] = future {
+    val ext = contentType.split("/")(1)
+
+    // Image I/O has built-in support for GIF, PNG, JPEG, BMP, and WBMP
+    List("GIF", "PNG", "JPEG", "BMP", "WBMP").contains(ext.toUpperCase) match {
+      case false =>
+        throw new IllegalArgumentException("Unmatchable content type")
+      case true =>
+        // resize the image
+        val bufferedImage: BufferedImage = ImageIO.read(file)
+
+        val resize = bufferedImage.getHeight() min bufferedImage.getWidth() min maxium_width_and_height
+
+        val thumbnail: BufferedImage = Scalr.resize(bufferedImage, resize)
+
+        val byteArrayOutputStream: ByteArrayOutputStream = new ByteArrayOutputStream()
+
+        ImageIO.write(thumbnail, ext, byteArrayOutputStream)
+
+        byteArrayOutputStream.toByteArray()
+    }
+  }
+
+
+  def upload(filePart: FilePart[TemporaryFile], path: String = "")(implicit storage: Storage): Future[String] = {
     storage.add(path + filePart.filename, filePart.ref.file)
+  }
+
+  def upload(fileName:String,byteArray:Array[Byte],contentType:String)(implicit storage: Storage): Future[String]= {
+    storage.add(fileName, new ByteArrayInputStream(byteArray), contentType)
   }
 
   def delete(fileName: String, path: String = "")(implicit storage: Storage): Future[String] = {
--- project/Build.scala
+++ project/Build.scala
@@ -4,12 +4,16 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "filemanager"
-  val appVersion      = "1.0-SNAPSHOT"
+  val appName = "filemanager"
+  val appVersion = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
     // Add your project dependencies here,
+    //Amazon S3 SDK
     "com.amazonaws" % "aws-java-sdk" % "1.0.002",
+
+    //imagescalr - Java Image Scaling Library
+    "org.imgscalr" % "imgscalr-lib" % "4.2",
     jdbc,
     anorm
   )
@@ -17,8 +21,8 @@ object ApplicationBuild extends Build {
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
     // Add your own project settings here      
-    organization   := "com.github.theodoreLee",  
-publishTo := Some(Resolver.file("Github Pages", Path.userHome / "git" / "theodoreLee.github.com" / "maven" asFile)(Patterns(true, Resolver.mavenStyleBasePattern)))
- )
+    organization := "com.github.theodoreLee",
+    publishTo := Some(Resolver.file("Github Pages", Path.userHome / "git" / "theodoreLee.github.com" / "maven" asFile)(Patterns(true, Resolver.mavenStyleBasePattern)))
+  )
 
 }
